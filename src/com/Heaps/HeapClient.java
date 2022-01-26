68e3b1c892766cf0beed833bfac90135508937fd
Atul Taneja <atultnj@gmail.com>
2022-01-26 18:35:30 +0530

    remove method in heap with edge cases

--- src/com/Heaps/HeapClient.java
+++ src/com/Heaps/HeapClient.java
@@ -46,12 +46,26 @@ class Heap{
     // HELPER METHODS
     //largerChildIndex
     private int largerChildIndex(int index){
+        if(!hasLeftChild(index))
+            return index;
+        //we fill the complete binary tree from left
+        //if node does'nt have right child it will have leftChild
+        if(!hasRightChild(index))
+            return leftChildIndex(index);
         return (leftChild(index)>rightChild(index)) ?
                 leftChildIndex(index) :
                 rightChildIndex(index) ;
     }
     // isValidParent
     private boolean isValidParent(int index){
+        //if it does'nt have left child it will not have right child
+        //it will be a valid parent
+        if(!hasLeftChild(index))
+            return true;
+        //if it does not have right child it will have left child
+        //check if leftChild is smaller than its parent .
+        if (!hasRightChild(index))
+            return items[index]>=leftChild(index);
         return items[index]>=leftChild(index) &&
                 items[index]>=rightChild(index);
     }
@@ -63,7 +77,11 @@ class Heap{
     private int leftChild(int index){
         return items[leftChildIndex(index)];
     }
-
+    //hasLeftChild(edge cases)
+    //if a node has a left child its index must be less than or equal to size
+    private boolean hasLeftChild(int index){
+        return leftChildIndex(index)<=size;
+    }
     //rightChildIndex
     private int rightChildIndex(int index){
         return (index+1)+2;
@@ -72,13 +90,23 @@ class Heap{
     private int rightChild(int index){
         return items[rightChildIndex(index)];
     }
+    //hasRighttChild(edge cases)
+    //if a node has a right child its index must be less than or equal to size
+    private boolean hasRightChild(int index){
+        return rightChildIndex(index)<=size;
+    }
     // swap
     private void swap(int first,int second){
         int temp = items[first];
         items[first] = items[second];
         items[second] = temp;
     }
-
+    //printHeap
+    public void printHeap(){
+        for(int i=0;i<size;i++)
+            System.out.print(items[i]+" ");
+        System.out.println();
+    }
 }
 public class HeapClient {
     public static void main(String[] args) {
@@ -88,6 +116,8 @@ public class HeapClient {
         heap.Insert(17);
         heap.Insert(4);
         heap.Insert(22);
+        heap.remove();
+        heap.printHeap();
         System.out.printf("done");
     }
 }
